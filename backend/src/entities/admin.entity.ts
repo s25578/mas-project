9875f5ca94c9da73332934451f564591a7b51ead
lululu <you@example.com>
2024-06-27 05:00:47 +0200

    some comments

--- backend/src/entities/admin.entity.ts
+++ backend/src/entities/admin.entity.ts
@@ -9,6 +9,15 @@ export class Admin extends User {
     @Column("simple-array")
     notificationPreferences: string[];
 
+    /**
+     * Create a new admin
+     * @param id number - ID of the admin
+     * @param name string - Name of the admin
+     * @param email string - Email of the admin
+     * @param password string - Password of the admin
+     * @param notificationPreferences Notification[] - Notification preferences of the admin
+     * @constructor
+    */
     constructor(id: number, name: string, email: string, password: string, notificationPreferences?: Notification[]) {
         super(id, name, email, password, UserRole.ADMIN);
         this.notificationPreferences = notificationPreferences || [];
